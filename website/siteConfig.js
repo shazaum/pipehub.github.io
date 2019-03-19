c6bf3d849bb5d64713c6845ecb0058c2286d3d31
Diego Bernardes <diego.bernardes@outlook.com>
2019-03-19 23:08:02 +0000

    Remove unnecessary pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,9 +40,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: 'doc1', label: 'Docs' },
-    { doc: 'doc4', label: 'API' },
     { page: 'help', label: 'Help' },
-    { blog: true, label: 'Blog' },
   ],
 
   // If you have users set above, you add it here:
